b9bed3c2320aedf389cfd8aedfc594f584617380
Miguel Muscat <miguelmuscat93@gmail.com>
2018-04-21 11:30:25 +0200

    Documented exceptions thrown for transformers

--- src/Transformer/TransformerInterface.php
+++ src/Transformer/TransformerInterface.php
@@ -2,6 +2,9 @@
 
 namespace RebelCode\EddBookings\RestApi\Transformer;
 
+use RebelCode\EddBookings\RestApi\Transformer\Exception\CouldNotTransformExceptionInterface;
+use RebelCode\EddBookings\RestApi\Transformer\Exception\TransformerExceptionInterface;
+
 /**
  * Something that can transform some source data into some output data.
  *
@@ -17,6 +20,9 @@ interface TransformerInterface
      * @param mixed $source The source data to transform.
      *
      * @return mixed The output data.
+     *
+     * @throws TransformerExceptionInterface If an error occurred during transformation.
+     * @throws CouldNotTransformExceptionInterface If the given source data could not be transformed.
      */
     public function transform($source);
 }
