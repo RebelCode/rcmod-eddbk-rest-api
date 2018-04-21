010534dbc4d83fff96a0de3487f3632752757f6c
Miguel Muscat <miguelmuscat93@gmail.com>
2018-04-21 13:28:29 +0200

    Added `AbstractWpRestApiHandler`

---
+++ src/Handlers/AbstractWpRestApiHandler.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace RebelCode\EddBookings\RestApi\Handlers;
+
+use Dhii\Exception\CreateInvalidArgumentExceptionCapableTrait;
+use Dhii\I18n\StringTranslatingTrait;
+use Dhii\Invocation\InvocableInterface;
+use Dhii\Util\Normalization\NormalizeArrayCapableTrait;
+use Exception;
+use stdClass;
+use Traversable;
+use WP_Error;
+use WP_REST_Request;
+use WP_REST_Response;
+
+/**
+ * Abstract and common functionality for WordPress REST API handlers.
+ *
+ * @since [*next-version*]
+ */
+abstract class AbstractWpRestApiHandler implements InvocableInterface
+{
+    /* @since [*next-version*] */
+    use NormalizeArrayCapableTrait;
+
+    /* @since [*next-version*] */
+    use CreateInvalidArgumentExceptionCapableTrait;
+
+    /* @since [*next-version*] */
+    use StringTranslatingTrait;
+
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     */
+    public function __invoke()
+    {
+        /* @var $request WP_REST_Request */
+        $request = func_get_arg(0);
+
+        try {
+            return $this->_handle($request);
+        } catch (Exception $exception) {
+            return new WP_Error($exception->getCode(), $exception->getMessage(), ['status' => 500]);
+        }
+    }
+
+    /**
+     * Handles the request and provides a response.
+     *
+     * @since [*next-version*]
+     *
+     * @param WP_REST_Request $request The request.
+     *
+     * @return WP_REST_Response|WP_Error The response data.
+     */
+    abstract protected function _handle(WP_REST_Request $request);
+}
