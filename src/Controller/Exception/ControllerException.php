f80472a63a07c356ce5e1b21c1bb1a79d56e8914
Miguel Muscat <miguelmuscat93@gmail.com>
2018-04-25 15:02:28 +0200

    Added controller exceptions

---
+++ src/Controller/Exception/ControllerException.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace RebelCode\EddBookings\RestApi\Controller\Exception;
+
+use Dhii\Exception\AbstractBaseException;
+use Throwable;
+
+/**
+ * An exception related to a controller.
+ *
+ * @since [*next-version*]
+ */
+class ControllerException extends AbstractBaseException
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     */
+    public function __construct($message = null, $code = null, Throwable $previous = null)
+    {
+        $this->_initParent($message, $code, $previous);
+    }
+}
---
+++ src/Controller/Exception/ControllerExceptionInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace RebelCode\EddBookings\RestApi\Controller\Exception;
+
+use Dhii\Exception\ThrowableInterface;
+
+/**
+ * An exception thrown in relation to a controller.
+ *
+ * @since [*next-version*]
+ */
+interface ControllerExceptionInterface extends ThrowableInterface
+{
+}
