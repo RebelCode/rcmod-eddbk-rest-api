ecc6ace33c4acf0ced779b3b26a51311b25a8987
Miguel Muscat <miguelmuscat93@gmail.com>
2018-04-25 16:54:42 +0200

    Added all methods to controller interface

--- src/Controller/ControllerInterface.php
+++ src/Controller/ControllerInterface.php
@@ -42,4 +42,46 @@ interface ControllerInterface
      *                                    {@see stdClass}, {@see ArrayAccess} or {@see ContainerInterface}.
      */
     public function post($params = []);
+
+    /**
+     * Updates resources entirely, based on given parameters.
+     *
+     * @since [*next-version*]
+     *
+     * @param array|stdClass|ArrayAccess|ContainerInterface $params The parameters.
+     *
+     * @throws ControllerExceptionInterface If an error occurred.
+     *
+     * @return array|stdClass|Traversable The response, as a traversable list consisting of container elements: array,
+     *                                    {@see stdClass}, {@see ArrayAccess} or {@see ContainerInterface}.
+     */
+    public function put($params = []);
+
+    /**
+     * Modifies resources, based on given parameters.
+     *
+     * @since [*next-version*]
+     *
+     * @param array|stdClass|ArrayAccess|ContainerInterface $params The parameters.
+     *
+     * @throws ControllerExceptionInterface If an error occurred.
+     *
+     * @return array|stdClass|Traversable The response, as a traversable list consisting of container elements: array,
+     *                                    {@see stdClass}, {@see ArrayAccess} or {@see ContainerInterface}.
+     */
+    public function patch($params = []);
+
+    /**
+     * Deletes resources, based on parameters.
+     *
+     * @since [*next-version*]
+     *
+     * @param array|stdClass|ArrayAccess|ContainerInterface $params The parameters.
+     *
+     * @throws ControllerExceptionInterface If an error occurred.
+     *
+     * @return array|stdClass|Traversable The response, as a traversable list consisting of container elements: array,
+     *                                    {@see stdClass}, {@see ArrayAccess} or {@see ContainerInterface}.
+     */
+    public function delete($params = []);
 }
