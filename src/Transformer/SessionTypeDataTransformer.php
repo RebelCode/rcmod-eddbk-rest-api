f38fc526206b5c9010fb471c16fe606c0c0c0d30
Miguel Muscat <miguelmuscat93@gmail.com>
2018-11-20 16:51:08 +0100

    Added transformer for session type data
    EDDBK-173

---
+++ src/Transformer/SessionTypeDataTransformer.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace RebelCode\EddBookings\RestApi\Transformer;
+
+use Dhii\Exception\CreateInvalidArgumentExceptionCapableTrait;
+use Dhii\I18n\StringTranslatingTrait;
+use Dhii\Transformer\TransformerInterface;
+use Dhii\Util\Normalization\NormalizeArrayCapableTrait;
+
+/**
+ * A transformer that transformers the data for session types.
+ *
+ * @since [*next-version*]
+ */
+class SessionTypeDataTransformer implements TransformerInterface
+{
+    /* @since [*next-version*] */
+    use NormalizeArrayCapableTrait;
+
+    /* @since [*next-version*] */
+    use CreateInvalidArgumentExceptionCapableTrait;
+
+    /* @since [*next-version*] */
+    use StringTranslatingTrait;
+
+    /**
+     * The transformer for transforming resource IDs into full resource data.
+     *
+     * @since [*next-version*]
+     *
+     * @var TransformerInterface
+     */
+    protected $resourcesT9r;
+
+    /**
+     * Constructor.
+     *
+     * @since [*next-version*]
+     *
+     * @param TransformerInterface $resourcesT9r The transformer for transforming resource IDs into full resource data.
+     */
+    public function __construct(TransformerInterface $resourcesT9r)
+    {
+        $this->resourcesT9r = $resourcesT9r;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     */
+    public function transform($source)
+    {
+        $result = $this->_normalizeArray($source);
+
+        if (isset($result['resources'])) {
+            foreach ($result['resources'] as $_key => $resourceId) {
+                $result['resources'][$_key] = $this->resourcesT9r->transform($resourceId);
+            }
+        }
+
+        return $result;
+    }
+}
